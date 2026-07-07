1a8f25fa2f5da2ca4933ab6814ca50801610d3a7
Throne0826 <2231537509@qq.com>
2026-07-07 11:13:08 +0800

    Improve code block highlighting

--- js/lib/highlight.js
+++ js/lib/highlight.js
@@ -1,4 +1,4 @@
-mixins.highlight = {
+﻿mixins.highlight = {
     data() {
         return { copying: false };
     },
@@ -10,21 +10,73 @@ mixins.highlight = {
         sleep(delay) {
             return new Promise((resolve) => setTimeout(resolve, delay));
         },
+        escapeHTML(value) {
+            return value.replace(/[&<>"']/g, (char) => {
+                const chars = {
+                    "&": "&amp;",
+                    "<": "&lt;",
+                    ">": "&gt;",
+                    '"': "&quot;",
+                    "'": "&#39;",
+                };
+                return chars[char];
+            });
+        },
+        normalizeLanguage(value) {
+            const raw = (value || "")
+                .toLowerCase()
+                .replace(/^language-/, "")
+                .replace(/^lang-/, "");
+            const aliases = {
+                "c++": "cpp",
+                cc: "cpp",
+                cxx: "cpp",
+                hpp: "cpp",
+                py: "python",
+                js: "javascript",
+                ts: "typescript",
+                sh: "bash",
+                shell: "bash",
+                plain: "plaintext",
+                text: "plaintext",
+            };
+            return aliases[raw] || raw || "plaintext";
+        },
+        getLanguageLabel(language) {
+            const labels = {
+                cpp: "C++",
+                javascript: "JavaScript",
+                typescript: "TypeScript",
+                python: "Python",
+                bash: "Bash",
+                plaintext: "Text",
+            };
+            return labels[language] || language.toUpperCase();
+        },
         highlight() {
             let codes = document.querySelectorAll("pre");
             for (let i of codes) {
                 let code = i.textContent;
-                let language = [...i.classList, ...i.firstChild.classList][0] || "plaintext";
+                let classes = [...i.classList];
+                if (i.firstElementChild) classes.push(...i.firstElementChild.classList);
+                let rawLanguage = classes.find((item) => /^lang(uage)?-/.test(item)) || classes[0] || "plaintext";
+                let language = this.normalizeLanguage(rawLanguage);
                 let highlighted;
                 try {
-                    highlighted = hljs.highlight(code, { language }).value;
+                    if (hljs.getLanguage(language)) {
+                        highlighted = hljs.highlight(code, { language }).value;
+                    } else {
+                        language = "plaintext";
+                        highlighted = this.escapeHTML(code);
+                    }
                 } catch {
-                    highlighted = code;
+                    language = "plaintext";
+                    highlighted = this.escapeHTML(code);
                 }
                 i.innerHTML = `
                     <div class="code-content hljs">${highlighted}</div>
-                    <div class="language">${language}</div>
-                    <div class="copycode">
+                    <div class="language">${this.getLanguageLabel(language)}</div>
+                    <div class="copycode" title="复制代码">
                         <i class="fa-solid fa-copy fa-fw"></i>
                         <i class="fa-solid fa-check fa-fw"></i>
                     </div>
